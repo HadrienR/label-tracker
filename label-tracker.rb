bf013d80467805a932bd6fde903fd4846315d57a
Hadrien Raffalli <hadrien.raffalli@gmail.com>
2016-12-09 12:09:38 +0900

    added running velocity for dynamic labels

--- label-tracker.rb
+++ label-tracker.rb
@@ -10,28 +10,30 @@ program :name, 'tracker-label'
 program :version, '0.0.1'
 program :description, 'Help track velocity per label on a Pivotal Tracker project.'
 
-command :decribe do |c|
-  c.syntax = 'test describe [options]'
-  c.summary = ''
-  c.description = ''
-  c.example 'description', 'command example'
-  c.option '--some-switch', 'Some switch that does something'
-  c.action do |args, options|
-    # Do something or c.when_called Test::Commands::Blob
-  end
+class Velocity
+  $total_velocity = 0
+  $labels_velocity = {}
+end
+
+class Me
+  $api_key = 0
 end
 
 def init
-  puts 'welcome to Label Tracker'
-  api_key = ask("Please insert your API Key:  ") { |q| q.echo = "*" }
-  find_projects (api_key)
+  puts 'Welcome to Label Tracker'
+  $api_key = ask("Please insert your API Token:  ") { |q| q.echo = "*" }
+  find_projects
 end
 
-def find_projects (api_key)
-  url = "https://www.pivotaltracker.com/services/v5/projects/"
-  head = {:'x-trackertoken'=> api_key}
+def tracker_get (url)
+  head = {:'x-trackertoken'=> $api_key}
   response = RestClient.get(url, headers = head)
-  projects = JSON.parse(response.body)
+  return JSON.parse(response.body)
+end
+
+def find_projects
+  url = "https://www.pivotaltracker.com/services/v5/projects/"
+  projects = tracker_get(url)
   rows = []
   projects.each_with_index do |project, i|
     rows << [i, project['name'], project['id']]
@@ -40,21 +42,12 @@ def find_projects (api_key)
   puts table
   puts 'Please select the project you want to analyze by typing the relevant number #, (ex: "0")'
   project_number = ask("Project # ?", Integer) { |q| q.in = 0..999 }
-  find_iterations(api_key, rows[project_number][2])
+  find_iterations(rows[project_number][2])
 end
 
-class Velocity
-  $total_velocity = 0
-  $ios = 0
-  $android = 0
-  $push = 0
-end
-
-def find_iterations (api_key, project_id)
+def find_iterations (project_id)
   url = "https://www.pivotaltracker.com/services/v5/projects/#{project_id}/iterations/?limit=10&offset=1"
-  head = {:'x-trackertoken'=> api_key}
-  response = RestClient.get(url, headers = head)
-  r = JSON.parse(response.body)
+  r = tracker_get(url)
   index = r.length
   last_three_iterations = [r[index - 1], r[index - 2], r[index - 3]]
   find_velocity (last_three_iterations)
@@ -66,14 +59,16 @@ def find_velocity (last_three)
     sum_points(item["stories"], item["team_strength"])
   end
 
-rows = []
-rows << ['RUNNING Velocity', $total_velocity / 3]
-rows << ['adjusted iOS velocity', $ios / 3]
-rows << ['adjusted Android velocity', $android / 3]
-rows << ['adjusted Push velocity', $push / 3]
-table = Terminal::Table.new :rows => rows
-puts table
+  rows = []
+  rows << ['RUNNING Velocity', $total_velocity / 3]
+  rows << :separator
 
+  $labels_velocity.each do |key, value|
+    rows << [key, value]
+  end
+
+  table = Terminal::Table.new :rows => rows
+  puts table
 end
 
 def sum_points(stories, ts)
@@ -90,15 +85,16 @@ def sum_points(stories, ts)
 end
 
 def platform_estimate(story, ts)
+
   estimate = story['estimate'] + story['estimate'] * (1-ts)
+
   story['labels'].each_with_index do |item, i|
-    if item['name'] == 'ios'
-      $ios += estimate
-    elsif item['name'] == 'android'
-      $android += estimate
-    elsif item['name'] == 'push server'
-      $push += estimate
+    if $labels_velocity[item['name']].is_a? Numeric
+        $labels_velocity[item['name']] += estimate
+    else
+      $labels_velocity[item['name']] = estimate
     end
+
   end
 end
 
